95361225c8a2ed83fd30e1c31141a8ebcf13866b
buaawelldon <buaawelldon@gmail.com>
2017-03-09 14:56:20 -0500

    update mnist cnn code

--- course_3_tf_mnist_cnn.py
+++ course_3_tf_mnist_cnn.py
@@ -16,8 +16,6 @@ batch_size = 100
 display_step = 1
 
 # Network Parameters
-n_hidden_1 = 256 # 1st layer number of features
-n_hidden_2 = 512 # 2nd layer number of features
 n_input = 784 # MNIST data input (img shape: 28*28)
 n_classes = 10 # MNIST total classes (0-9 digits)
 
