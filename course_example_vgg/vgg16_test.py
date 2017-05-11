287b612ac05cdb0afe8edfc0f3e31105a8cc7d6e
Huang Hao <huang1hao@gmail.com>
2017-05-11 23:30:48 +0800

    Print top5 labels for prediction from vgg16

--- course_example_vgg/vgg16_test.py
+++ course_example_vgg/vgg16_test.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 import vgg16
 import utils
+from imagenet1000_clsid_to_human import labels
 
 img1 = utils.load_image("./test_data/tiger.jpeg")
 img2 = utils.load_image("./test_data/puzzle.jpeg")
@@ -11,6 +12,9 @@ batch1 = img1.reshape((1, 224, 224, 3))
 batch2 = img2.reshape((1, 224, 224, 3))
 
 batch = np.concatenate((batch1, batch2), 0)
+
+def percent(v):
+    return '%.2f%%' % (v * 100)
 
 # with tf.Session(config=tf.ConfigProto(gpu_options=(tf.GPUOptions(per_process_gpu_memory_fraction=0.7)))) as sess:
 with tf.device('/cpu:0'):
@@ -23,4 +27,8 @@ with tf.device('/cpu:0'):
             vgg.build(images)
 
         prob = sess.run(vgg.prob, feed_dict=feed_dict)
-        print(prob)
+        for i, p in enumerate(prob):
+            v = sess.run(tf.nn.top_k(p, 5))
+            print('-'*4)
+            for j, k in enumerate(v.indices):
+                print(labels[k], ':', percent(v.values[j]))
